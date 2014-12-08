a12c7833ec44b676eff7fe86b2cc889d29cfc70b
Prenom NOM <alexisgermain1@Macintosh.local>
2014-12-08 15:04:46 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/PeevedPenguins.approj/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Alexis Germain on 08/12/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
