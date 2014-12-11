5df7c803656021bee0caf08092c106c28e0d6498
Prenom NOM <alexisgermain1@Macintosh.local>
2014-12-11 15:08:18 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/PeevedPenguins.approj/Gameplay.m
+++ PeevedPenguins.spritebuilder/PeevedPenguins.approj/Gameplay.m
@@ -48,9 +48,11 @@
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     [_contentNode runAction:follow];
+    
+}
 
     - (void)retry {
         // reload this level
         [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
-    }}
+    }
 @end
