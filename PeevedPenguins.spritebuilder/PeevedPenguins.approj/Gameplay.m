0089d2010991bd6602188672a8b6d36114248b9f
Prenom NOM <alexisgermain1@Macintosh.local>
2014-12-11 14:56:29 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/PeevedPenguins.approj/Gameplay.m
+++ PeevedPenguins.spritebuilder/PeevedPenguins.approj/Gameplay.m
@@ -11,7 +11,9 @@
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
-    CCNode *_levelNode; }
+    CCNode *_levelNode;
+    CCNode *_contentNode;
+}
 
 // is called when CCB file has completed loading
  - (void)didLoadFromCCB {
@@ -45,7 +47,7 @@
     // ensure followed object is in visible are when starting
     self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];}
+    [_contentNode runAction:follow];
 
 - (void)retry {
     // reload this level
